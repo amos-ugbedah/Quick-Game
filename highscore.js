c5fcedeb4ad732df21cc3fd4e6aaa38a776dcc53
Ugbedah Amos <ugbedahamos@gmail.com>
2025-02-02 13:12:57 +0100

    new update

--- highscore.js
+++ highscore.js
@@ -1,10 +1,7 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScores.map((score) => {
-  console.log(
-    <li class="high-score">
-      '${score.name}-${score.score}'
-    </li>
-  );
-});
+// Generate the list items for high scores
+highScoresList.innerHTML = highScores
+  .map((score) => `<li class="high-score">${score.name} - ${score.score}</li>`)
+  .join("");
